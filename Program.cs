c3784691f7ef00e068a1dd3edcfcb14480fd55fd
kubuzbusra <kubuzbusra@gmail.com>
2016-11-22 13:58:15 +0300

    added project

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace SayindanKelimeye.WFA
         /// </summary>
         [STAThread]
         static void Main()
+
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
