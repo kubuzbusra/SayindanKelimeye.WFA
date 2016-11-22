a8324ceef0d9f06561ce5da6087ae317e596d725
sametabay <sametabay37@hotmail.com>
2016-11-22 13:55:49 +0300

    Add project files.

---
+++ SayidanKelimeyeTextBox.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SayindanKelimeye.WFA
+{
+    partial class SayidanKelimeyeTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.txtYazi = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtSayi);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtYazi);
+            this.splitContainer1.Size = new System.Drawing.Size(300, 85);
+            this.splitContainer1.SplitterDistance = 43;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSayi.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi.Location = new System.Drawing.Point(0, 0);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(300, 36);
+            this.txtSayi.TabIndex = 0;
+            this.txtSayi.TextChanged += new System.EventHandler(this.txtSayi_TextChanged);
+            this.txtSayi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayi_KeyPress);
+            // 
+            // txtYazi
+            // 
+            this.txtYazi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtYazi.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYazi.ForeColor = System.Drawing.Color.Maroon;
+            this.txtYazi.Location = new System.Drawing.Point(0, 0);
+            this.txtYazi.Name = "txtYazi";
+            this.txtYazi.ReadOnly = true;
+            this.txtYazi.Size = new System.Drawing.Size(300, 36);
+            this.txtYazi.TabIndex = 0;
+            // 
+            // SayidanKelimeyeTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "SayidanKelimeyeTextBox";
+            this.Size = new System.Drawing.Size(300, 85);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.TextBox txtYazi;
+    }
+}
---
+++ SayidanKelimeyeTextBox.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayindanKelimeye.WFA
+{
+    public delegate void MaximumValueHandler(string message);
+    public partial class SayidanKelimeyeTextBox : UserControl
+    {
+        public SayidanKelimeyeTextBox()
+        {
+            InitializeComponent();
+        }
+        public bool SinifPropYazmayiBiliyorMu { get; set; }
+        public int Sayi { get; set; }
+        public event MaximumValueHandler MaxValueOver;
+        public string Yazi
+        {
+            get
+            {
+                return txtYazi.Text;
+            }
+        }
+        string OkunusunuEkranaYaz(int[] basamaklar)
+        {
+            string[] birler = { "", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz" };
+            string[] onlar = { "", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Atmış", "Yetmiş", "Seksen", "Doksan" };
+            string yuzler = "Yüz";
+            string binler = "Bin";
+            string cikti = string.Empty;
+
+            if (basamaklar[3] == 1)
+                cikti += binler;
+            else if (basamaklar[3] != 0)
+                cikti += birler[basamaklar[3]] + binler;
+
+            if (basamaklar[2] == 1)
+                cikti += yuzler;
+            else if (basamaklar[2] != 0)
+                cikti += birler[basamaklar[2]] + yuzler;
+
+            if (basamaklar[0] == 0 && basamaklar[1] == 0 && basamaklar[2] == 0 & basamaklar[3] == 0)
+                cikti = "Sıfır";
+            else
+                cikti += onlar[basamaklar[1]] + birler[basamaklar[0]];
+            return cikti;
+        }
+        int[] Basamaklandir(int girilenSayi)
+        {
+            int[] basamaklar = new int[4];
+            basamaklar[0] = girilenSayi % 10; // birler basamağı
+            basamaklar[1] = (girilenSayi / 10) % 10; //onlar basamağı
+            basamaklar[2] = (girilenSayi / 100) % 10; // yüzler basamağı
+            basamaklar[3] = (girilenSayi / 1000) % 10; // binler basamağı
+            return basamaklar;
+        }
+        int SayiOku()
+        {
+            int sayi = 0;
+            try
+            {
+                sayi = int.Parse(txtSayi.Text);
+                if (sayi < 0 || sayi > 9999)
+                {
+                    MaxValueOver("Maximum değeri aştınız");
+                    txtSayi.Text = "0";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtSayi.Text = "0";
+            }
+            return sayi;
+        }
+        private void txtSayi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\b')
+                return;
+            else if (!char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+        private void txtSayi_TextChanged(object sender, EventArgs e)
+        {
+            if (txtSayi.Text.Length > 4)
+                MaxValueOver("Maksimum Sayıyı aştınız");
+            txtYazi.Text = OkunusunuEkranaYaz(Basamaklandir(SayiOku()));
+        }
+    }
+}
